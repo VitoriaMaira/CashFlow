c2ba277075e8be49d9b3d8a258c8defe8fe3c68e
vitoria <vitoria.maira@souunit.com.br>
2026-07-08 07:19:16 -0300

    feat: registrando DbContext como Injeção de dependencias

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,13 +1,26 @@
 ﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Infrastructure;
+
 public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services)
     {
+        AddRepositories(services);
+        AddDbContext(services);
+    }
+
+    private static void AddRepositories(this IServiceCollection services)
+    {
         services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+
+    private static void AddDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
     }
 
 }
